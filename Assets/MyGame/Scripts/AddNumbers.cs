3b899ef35aed29c69e9d8e043869628eeb32bc7c
smeerws <hello@smeerws.at>
2019-11-09 12:28:37 +0100

    create script add numbers

---
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddNumbers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 3;
+        int b = 4;
+
+        Debug.Log("a = " + a);
+        Debug.Log("b = " + b);
+        Debug.Log("a + b = " + (a + b));
+
+        Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
